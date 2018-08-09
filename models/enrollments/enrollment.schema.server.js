0c847a0e96f50d5c9733fa81a54e70a4768f6824
Melina <sayeghmelina@gmail.com>
2018-08-08 23:27:02 -0400

    added grade to enrollment schema

--- models/enrollments/enrollment.schema.server.js
+++ models/enrollments/enrollment.schema.server.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 
 const enrollmentSchema = mongoose.Schema({
-    student: {type: mongoose.Schema.Types.ObjectId,
-        ref: 'UserModel'},
-    section: {type: mongoose.Schema.Types.ObjectId,
-        ref: 'SectionModel'}
+    student: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'UserModel'
+    },
+    section: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'SectionModel'
+    },
+    grade: String
 }, {collection: 'enrollments'});
 
 module.exports = enrollmentSchema;
--- services/section.service.server.js
+++ services/section.service.server.js
@@ -3,9 +3,30 @@ module.exports = app => {
     const sectionModel = require('../models/sections/section.model.server');
 
     app.put('/api/section/:sectionId/enroll', (req, res) => {
+
+
+        const sectionId = req.params['sectionId'];
         const currentUser = req.session['currentUser'];
+        const studentId = currentUser._id;
+        const enrollment = {
+            student: studentId,
+            section: sectionId
+        };
+
+        sectionModel
+            .decrementSectionSeats(sectionId)
+            .then(() => {
+                return enrollmentModel.enrollStudentInSection(enrollment)
+            })
+            .then(function (enrollment) {
+                res.json(enrollment);
+            });
+
+
         sectionModel.enroll(currentUser._id, req.params['sectionId'])
             .then(status => res.sendStatus(200))
+
+
     });
 
     app.get('/api/section', (req, res) =>
