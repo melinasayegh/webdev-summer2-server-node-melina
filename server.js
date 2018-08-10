98838558d225356974c8111457004dbfa3004b17
Melina <sayeghmelina@gmail.com>
2018-08-10 05:15:02 -0400

    CORS for heroku

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(function(req, res, next) {
     //    res.setHeader('Access-Control-Allow-Origin', origin);
     //}
     //res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-    res.header("Access-Control-Allow-Origin", "https://webdev-client-angular-melina.herokuapp.com/");
+    res.header("Access-Control-Allow-Origin", "https://webdev-client-angular-melina.herokuapp.com");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
